04f0424fa92a8174329f16527683278126c6680d
Steve McDougall <juststevemcd@gmail.com>
2021-12-02 19:39:03 +0000

    Workflow Builder test

--- tests/WorkflowBuilderTest.php
+++ tests/WorkflowBuilderTest.php
@@ -1,1 +1,14 @@
 <?php
+
+declare(strict_types=1);
+
+use JustSteveKing\Workflow\Loaders\YamlLoader;
+use JustSteveKing\Workflow\WorkflowBuilder;
+
+it('can create a workflow from an array', function () {
+    WorkflowBuilder::make(
+        payload: YamlLoader::load(
+           path: __DIR__ . '/Fixtures/test.yaml',
+       ),
+    );
+});
