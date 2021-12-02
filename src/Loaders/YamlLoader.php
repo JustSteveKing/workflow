050c00f1c875b07f944c3862ffcbe63892c927c2
Steve McDougall <juststevemcd@gmail.com>
2021-12-02 19:20:27 +0000

    Building loaders and workflow runner

--- src/Loaders/YamlLoader.php
+++ src/Loaders/YamlLoader.php
@@ -2,11 +2,21 @@
 
 declare(strict_types=1);
 
-
 namespace JustSteveKing\Workflow\Loaders;
 
+use JustSteveKing\Workflow\Contracts\LoaderContract;
+use Symfony\Component\Yaml\Yaml;
 
-class YamlLoader
+class YamlLoader implements LoaderContract
 {
-
+    /**
+     * @param string $path
+     * @return array
+     */
+    public static function load(string $path): array
+    {
+        return (array) Yaml::parseFile(
+            filename: $path,
+        );
+    }
 }
--- src/WorkflowRunner.php
+++ src/WorkflowRunner.php
@@ -2,11 +2,24 @@
 
 declare(strict_types=1);
 
-
 namespace JustSteveKing\Workflow;
 
+use JustSteveKing\Workflow\Contracts\WorkflowContract;
 
 class WorkflowRunner
 {
+    protected function __construct(
+        protected array $workflows,
+    ) {}
 
+    /**
+     * @param array<int,WorkflowContract> $workflows
+     * @return static
+     */
+    public static function build(array $workflows = []): static
+    {
+        return new static(
+            workflows: $workflows
+        );
+    }
 }
